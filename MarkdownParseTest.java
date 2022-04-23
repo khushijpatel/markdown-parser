9d894a446e325d1ccd171c7315725dcca3d0a090
khushijpatel <103156629+khushijpatel@users.noreply.github.com>
2022-04-23 12:37:43 -0700

    add tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -11,6 +11,7 @@ public class MarkdownParseTest{
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+    /*
     @Test
     public void testLinks() throws IOException{
         Path fileName = Path.of("test-file.md");
@@ -20,16 +21,41 @@ public class MarkdownParseTest{
         result.add(0, "https://something.com");
         result.add(1, "some-thing.html");
         assertEquals(links, result);
-
     }
+    */
 
     public void testLinks1() throws IOException{
+        Path fileName = Path.of("tester.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> result = new ArrayList<String>();
+        result.add(0, "www.google.com");
+        result.add(1, "https://cse.ucsd.edu/");
+        result.add(2, "http://www.sysnet.ucsd.edu/~voelker/pubcom/logo/CSELogo_4Cv.jpg");
+        assertEquals(links, result);
+    }
+
+    public void testLinks2() throws IOException{
         Path fileName = Path.of("tester2.md");
         String content = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(content);
         ArrayList<String> result = new ArrayList<String>();
         result.add(0, "www.google.com");
         result.add(1, "https://cse.ucsd.edu/");
+        assertEquals(links, result);
+    }
+
+
+    public void testLinks3() throws IOException{
+        Path fileName = Path.of("tester3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> result = new ArrayList<String>();
+        result.add(0, "some-thing.html");
+        result.add(1, "https://google.com");
+        result.add(2, "some-thing.html");
+        result.add(3, "https://google.com");
+        result.add(4, "some-thing.html");
         assertEquals(links, result);
     }
 }
